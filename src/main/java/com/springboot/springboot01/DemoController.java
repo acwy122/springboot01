a3a80f823971c9724160a773d156fc56800d6395
xurb <xurongbiao@hmtim.com>
2021-01-21 15:14:26 +0800

    spring 001

---
+++ src/main/java/com/springboot/springboot01/DemoController.java
@@ -0,0 +1,14 @@
+package com.springboot.springboot01;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @RequestMapping("/index")
+    public String index(String posId) {
+        return "hello springboot!" + posId;
+    }
+
+}
