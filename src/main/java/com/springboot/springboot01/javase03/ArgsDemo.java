344df9890c22127fb2848e7580c3e20fadd8c990
xurb <xurongbiao@hmtim.com>
2022-03-16 16:22:34 +0800

    javase02,javase03

---
+++ src/main/java/com/springboot/springboot01/javase03/ArgsDemo.java
@@ -0,0 +1,10 @@
+package com.springboot.springboot01.javase03;
+
+public class ArgsDemo{
+	
+	public static void main(String[] args){
+		for(int i = 0;i<args.length;i++){
+			System.out.println(args[i]);
+		}
+	}
+}
