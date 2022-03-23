ca195517cf92a20409eb84de3f67540dae14121c
xurb <xurongbiao@hmtim.com>
2022-03-23 16:16:53 +0800

    增加springmvc

--- src/main/java/com/springboot/springboot01/javase/javase08/demo/ThreadDemo2.java
+++ src/main/java/com/springboot/springboot01/javase/javase08/demo/ThreadDemo2.java
@@ -1,4 +1,20 @@
 package com.springboot.springboot01.javase.javase08.demo;
 
-public class ThreadDemo2 {
+public class ThreadDemo2 extends Thread {
+
+    @Override
+    public void run(){
+        for(int i=0;i<10;i++) {
+            System.out.println(Thread.currentThread().getName()+"==========="+i);
+        }
+    }
+
+    public static void main(String[] args) {
+        ThreadDemo2 t = new ThreadDemo2();
+        t.start();
+        for(int i=0;i<5;i++){
+            System.out.println(Thread.currentThread().getName()+"---------"+i);
+        }
+    }
+
 }
